f5e1600d69855f121a2c36c9b4ba912068220269
Yin Lee <leesrrh@gmail.com>
2023-11-17 16:05:57 +0900

    create new item default wallet

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,8 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
 
-    # @wallet = Wallet.find(params[:wallet_id])
+    @wallet = Wallet.find(params[:wallet_id])
+    @item.wallet_id = params[:wallet_id]
 
     if @item.save
       redirect_to wallet_path(@wallet)
